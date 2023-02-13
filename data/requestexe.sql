34d410b1cde062dfcb6f13517cc368ed8e8dafbd
mikhawa <michaeljpitz@gmail.com>
2023-02-13 14:57:12 +0100

    m2m

--- data/requestexe.sql
+++ data/requestexe.sql
@@ -1,36 +1,62 @@
 # Séléction de tous les champs de la table category
 SELECT * FROM category;
 # Séléction des champs id et title de la table category
-
+SELECT id, title FROM category;
 # Séléction des champs id et title de la table category classé par id ascendant
-
+SELECT id, title FROM category ORDER BY id ASC;
 # Séléction des champs id (renommé en idcateg) et title (renommé en titlecateg) de la table category classé par id ascendant
-
+SELECT id AS idcateg, title as titlecateg FROM category ORDER BY id ASC;
 
 
 # Séléction de tous les champs de la table post
 SELECT * FROM post;
 # Séléction des champs id, title et datecreate de la table post
-
+SELECT id, title, datecreate FROM post;
 # Séléction des champs id, title et datecreate de la table post ordonnés par datecreate descendante
-
+SELECT id, title, datecreate FROM post ORDER BY datecreate DESC;
 # Séléction des champs id (renommé en idpost), title (renommé en titlepost) et datecreate de la table post ordonnés par datecreate descendante
+SELECT id AS idpost, title AS titlepost, datecreate FROM post ORDER BY datecreate DESC;
 
 # Séléction des champs id (renommé en idpost), title (renommé en titlepost), 255 caractères de content (renommé en contentshort) et datecreate de la table post ordonnés par datecreate descendante
+SELECT id AS idpost, title AS titlepost, LEFT(content,255) AS contentshort, datecreate FROM post ORDER BY datecreate DESC;
 
 # Séléction des champs id (renommé en idpost), title (renommé en titlepost), 255 caractères de content (renommé en contentshort) et datecreate de la table post quand visible vaut 1 ordonnés par datecreate descendante
+SELECT id AS idpost, title AS titlepost, LEFT(content,255) AS contentshort, datecreate 
+FROM post 
+WHERE visible = 1 
+ORDER BY datecreate DESC;
 
 # Séléction des champs id (renommé en idpost), title (renommé en titlepost), 255 caractères de content (renommé en contentshort) et datecreate de la table post quand user_id n'est pas NULL (voir commande SQL: isnull())  ordonnés par datecreate descendante
+SELECT 
+    id AS idpost,
+    title AS titlepost,
+    LEFT(content, 255) AS contentshort,
+    datecreate
+FROM
+    post
+WHERE
+    user_id IS NOT NULL
+ORDER BY datecreate DESC;
 
 # Séléction des champs id (renommé en idpost), title (renommé en titlepost), 255 caractères de content (renommé en contentshort) et datecreate de la table post quand user_id n'est pas NULL (voir commande SQL: isnull()) ET que visible vaut 1 ordonnés par datecreate descendante
+SELECT 
+    id AS idpost,
+    title AS titlepost,
+    LEFT(content, 255) AS contentshort,
+    datecreate
+FROM
+    post
+WHERE
+    user_id IS NOT NULL AND visible=1
+ORDER BY datecreate DESC;
 
 
 # Séléction de tous les champs de la table user
 SELECT * FROM user;
 # Séléction des champs id, username, userscreen de la table user classés par userscreen ascendant
-
+SELECT id, username, userscreen FROM user ORDER BY userscreen ASC;
 # Séléction des champs id, username, userscreen de la table user quand actif vaut 1 classés par userscreen ascendant
-
+SELECT id, username, userscreen FROM user WHERE actif=1 ORDER BY userscreen ASC;
 
 #
 # JOINTURES INTERNES [INNER] - INNER JOIN ou JOIN
@@ -39,20 +65,39 @@ SELECT * FROM user;
 
 # Séléction des champs id, title et datecreate de la table post, AVEC le usercreen de la table user SEULEMENT si le lien existe, ordonnés par datecreate ascendante
 # Il faut utiliser le nom des tables pour éviter qu'il y ai une collision d'id !
+SELECT post.id, post.title, post.datecreate, user.userscreen 
+FROM post
+INNER JOIN user
+	ON user.id = post.user_id
+    ORDER BY post.datecreate ASC;
 
 
 # idem avec alias internes (alias de tables)
-
+SELECT p.id, p.title, p.datecreate, u.userscreen 
+FROM post p
+INNER JOIN user u
+	ON u.id = p.user_id
+    ORDER BY p.datecreate ASC;
 
 #
 # JOINTURES EXTERNES [OUTER] - LEFT ou RIGHT JOIN
 #
 
 # Séléction des champs id, title et datecreate de la table post, AVEC le usercreen de la table user MEME  si le lien N'EXISTE PAS (tous les post), ordonnés par datecreate ascendante
+SELECT p.id, p.title, p.datecreate, u.userscreen 
+FROM post p
+	LEFT JOIN user u
+		ON p.user_id = u.id
+ORDER BY p.datecreate ASC;
+
 
 
 # Séléction des champs id, title et datecreate de la table post, AVEC le usercreen de la table user DANS TOUS LES CAS même si le lien N'EXISTE PAS (tous les user), ordonnés par datecreate ascendante
-
+SELECT p.id, p.title, p.datecreate, u.userscreen 
+FROM post p
+	RIGHT JOIN user u
+		ON p.user_id = u.id
+ORDER BY p.datecreate ASC;
 
 #
 # JOINTURES sur M2M (many to many)
