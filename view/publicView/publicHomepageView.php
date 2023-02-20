182d709e4aa4f69bd18ede7549a8ce26719f4e80
mikhawa <michaeljpitz@gmail.com>
2023-02-20 21:04:04 +0100

    separate footer

--- view/publicView/publicHomepageView.php
+++ view/publicView/publicHomepageView.php
@@ -75,13 +75,8 @@ include "inc/publicMenu.php";
 
 </div>
 
-<!-- Footer-->
-<footer class="py-5 bg-dark">
-    <div class="container"><p class="m-0 text-center text-white">Copyright &copy; CF2m <?=date("Y")?></p></div>
-</footer>
-<!-- Bootstrap core JS-->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-<!-- Core theme JS-->
-<script src="js/scripts.js"></script>
+        <?php
+        include "inc/publicFooter.php";
+        ?>
 </body>
 </html>
