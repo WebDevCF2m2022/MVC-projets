213f14b2212176a405558526d0eaf76c89da7edb
mikhawa <michaeljpitz@gmail.com>
2023-03-06 14:25:33 +0100

    MVC-projets\view\publicView\inc\menuPublicView.php

--- view/publicView/404View.php
+++ view/publicView/404View.php
@@ -12,26 +12,11 @@
     <link href="css/styles.css" rel="stylesheet" />
 </head>
 <body>
-<!-- Responsive navbar-->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-        <a class="navbar-brand" href="./">DB - BDD : Les bases de données</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item"><a class="nav-link active" href="./">Accueil</a></li>
-                <?php
-                foreach($recupMenu as $item):
-                ?>
-<li class="nav-item"><a class="nav-link" href="?categoryId=<?=$item['id']?>"><?=$item['title']?></a></li>
-                <?php
-                endforeach;
-                ?>
-            </ul>
-        </div>
-    </div>
-</nav>
-<!-- Page content-->
+<?php
+// fonctionne pour les include / require - chemin relatif
+include_once "inc/menuPublicView.php";
+
+?>
 <div class="container mt-5">
     <div class="row">
         <div class="col-lg-8">
